3e6b3983e17ca7a008934e14209885b13c2a3693
javiberd <javierberdeciot@gmail.com>
2019-03-24 19:12:40 +0100

    Documentation improvements.

--- src/matrix.h
+++ src/matrix.h
@@ -1,9 +1,11 @@
 /**
   @file matrix.h
 
-  This is something
+  File containing all functions applicable to Matrix objects.
 
-  Author: Javier Berdecio Trigueros
+  In OOP these are called the methods of the class.
+
+  @author Javier Berdecio Trigueros
 */
 
 #ifndef MATRIX_H_
@@ -21,22 +23,22 @@ typedef struct _Matrix Matrix;
                 NULL, matrix will be filled with 0's.
   @return       A new matrix, or NULL if it was not possible to allocate memory.
 */
-Matrix *init  (int           nrow,
-               int           ncol,
-               const int     mat[nrow][ncol]);
+Matrix *Matrix_init     (int           nrow,
+                         int           ncol,
+                         const int     mat[nrow][ncol]);
 
 /**
-  Sets the value of the position (i, j) of the matrix to the value of parameter
-  \value.
+  Sets the value of the position (i, j) of the matrix to the value passed as
+  argument.
   @param matrix Matrix to be modified.
   @param i      Row of the matrix.
   @param j      Column of the matrix.
   @param value  Value to be set.
 */
-void    set      (Matrix       *matrix,
-                  int           i,
-                  int           j,
-                  int           value);
+void    Matrix_set      (Matrix       *matrix,
+                         int           i,
+                         int           j,
+                         int           value);
 
 /**
   Gets the value in the position (i, j) of the matrix.
@@ -45,77 +47,90 @@ void    set      (Matrix       *matrix,
   @param j      Column of the matrix.
   @return       The value in position (i, j) of the matrix.
 */
-int     get      (const Matrix *matrix,
-                  int           i,
-                  int           j);
+int     Matrix_get      (const Matrix *matrix,
+                         int           i,
+                         int           j);
 /**
   Adds matrix_2 to matrix_1, result is saved in matrix_1.
   @param matrix_1 One of the matrices to operate with. This will contain the
-                  resulting matrix.
+  resulting matrix.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix (matrix_1).
+  @return The resulting matrix (matrix_1) or NULL if the matrices differ on
+  number of colums or rows.
 */
-Matrix *add      (Matrix       *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_add      (Matrix       *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Adds matrix_2 to matrix_1, result is returned in a newly allocated matrix.
+  The two characters suffix of this function's name is "rn". This is to
+  emphasize in that the function 'r'eturns a 'n'ewly allocated matrix.
   @param matrix_1 One of the matrices to operate with.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix.
+  @return The resulting matrix or NULL if the matrices differ on number of
+  colums or rows.
 */
-Matrix *addn     (const Matrix *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_addrn    (const Matrix *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Subtracts matrix_2 to matrix_1, result is saved in matrix_1.
   @param matrix_1 One of the matrices to operate with. This will contain the
-                  resulting matrix.
+  resulting matrix.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix (matrix_1).
+  @return The resulting matrix (matrix_1) or NULL if the matrices differ on
+  number of colums or rows.
 */
-Matrix *sub      (Matrix       *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_sub      (Matrix       *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Subtracts matrix_2 to matrix_1, result is returned in a newly allocated
-  matrix.
+  matrix. The two characters suffix of this function's name is "rn". This is to
+  emphasize in that the function 'r'eturns a 'n'ewly allocated matrix.
   @param matrix_1 One of the matrices to operate with.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix.
+  @return The resulting matrix or NULL if the matrices differ on
+  number of colums or rows.
 */
-Matrix *subn     (const Matrix *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_subrn    (const Matrix *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Multiplies matrix_1 and matrix_2 element by element and then returns
-  that matrix.
+  that matrix. This operation is known as Hadamard product of matrices.
   @param matrix_1 One of the matrices to operate with.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix.
+  @return The resulting matrix or NULL if the matrices differ on
+  number of colums or rows.
 */
-Matrix *mul      (Matrix *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_mul      (Matrix       *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Multiplies matrix_1 and matrix_2 element by element, result is returned in
-  a newly allocated matrix.
+  a newly allocated matrix. This operation is known as Hadamard product of
+  matrices.The two characters suffix of this function's name is "rn". This is
+  to emphasize in that the function 'r'eturns a 'n'ewly allocated matrix.
   @param matrix_1 One of the matrices to operate with.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix.
+  @return The resulting matrix or NULL if the matrices differ on
+  number of colums or rows.
 */
-Matrix *muln     (const Matrix *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_mulrn    (const Matrix *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Multiplies matrix_1 and matrix_2 with the naive method and then returns
-  that matrix.
+  that matrix. The one character suffix of this function's name is "n". This
+  is to emphasize in that the 'n'aive method is of multiplication is used.
   @param matrix_1 One of the matrices to operate with.
   @param matrix_2 One of the matrices to operate with.
-  @return The resulting matrix.
+  @return The resulting matrix or NULL if the number of columns of matrix_1
+  differs of the number of rows of matrix_2.
 */
-Matrix *mulp     (const Matrix *matrix_1,
-                  const Matrix *matrix_2);
+Matrix *Matrix_muln     (const Matrix *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Compares two matrices, 1 is returned if both represent the same matrix. If
@@ -124,34 +139,34 @@ Matrix *mulp     (const Matrix *matrix_1,
   @param matrix_2 One of the matrices to compare with.
   @return The result of the comparison.
 */
-int     equals   (const Matrix *matrix_1,
-                  const Matrix *matrix_2);
+int     Matrix_equals   (const Matrix *matrix_1,
+                         const Matrix *matrix_2);
 
 /**
   Returns a string that represents the matrix.
   @param matrix Matrix to be represented as a string.
   @return The representation of the matrix as a string.
 */
-char   *str      (const Matrix *matrix);
+char   *Matrix_str      (const Matrix *matrix);
 
 /**
   Returns the number of rows that the matrix have.
   @param matrix A matrix.
   @return Number of rows.
 */
-int     get_nrow (const Matrix *matrix);
+int     Matrix_get_nrow (const Matrix *matrix);
 
 /**
   Returns the number of columns that the matrix have.
   @param matrix A matrix.
   @return Number of columns.
 */
-int     get_ncol (const Matrix *matrix);
+int     Matrix_get_ncol (const Matrix *matrix);
 
 /**
   Frees all memory allocated.
   @param Matrix to be freed.
 */
-void    del    (Matrix       *matrix);
+void    Matrix_del      (Matrix       *matrix);
 
 #endif
